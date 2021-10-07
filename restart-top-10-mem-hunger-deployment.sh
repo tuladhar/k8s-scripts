752a03eb6ecfedadcdb11656f901eea4de8779a5
Puru <tuladharpuru@gmail.com>
2021-10-07 12:29:30 +0545

    Create restart-top-10-mem-hunger-deployment.sh

---
+++ restart-top-10-mem-hunger-deployment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TOP=10
+NODE=$1
+POD_PREFIX=trinity
+
+LINES="$(kubectl describe node $NODE | grep $POD_PREFIX | awk '{print $1 " " $2 " " $5}' | sort | uniq | sort -rnk3 | head -n $TOP | tr ' ' '_')"
+
+for LINE in ${LINES[@]}
+do
+  NAMESPACE=$(echo $LINE | awk -F_ '{print $1}')
+  POD_NAME=$(echo $LINE | awk -F_ '{print $2}')
+  MEM_USAGE=$(echo $LINE | awk -F_ '{print $3}')
+  SERVICE_NAME=$(echo $POD_NAME | awk -F- '{print $2}')
+  DEPLOYMENT_NAME=trinity-$SERVICE_NAME-deploy
+  echo "kubectl -n $NAMESPACE rollout restart deploy $DEPLOYMENT_NAME # MEM: $MEM_USAGE"
+done
